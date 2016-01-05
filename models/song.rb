435214deac65056c9d34ac79f77ef9114322a236
Dave Koenig <davidjosephkoenig@gmail.com>
2016-01-05 09:33:34 -0500

    write method to format time

--- models/song.rb
+++ models/song.rb
@@ -1,4 +1,9 @@
 class Song < ActiveRecord::Base
 	belongs_to(:album)
 
+	def time
+		sec = self.time_in_seconds % 60
+		min = (self.time_in_seconds - sec) / 60
+		"#{min.to_s}:#{'%02d' % sec}"
+	end
 end
