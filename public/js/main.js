0bd8a17565a288e0bb9f0cd0261d3e7ffbf839e9
Dave Koenig <davidjosephkoenig@gmail.com>
2016-01-06 09:49:31 -0500

    set nav height to screen height

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,6 @@
-alert('yo');
+function setNavHeightToScreenHeight() {
+	var nav = document.getElementsByTagName("nav")[0];
+	nav.style.height = window.innerHeight + "px";
+}
+
+document.addEventListener("DOMContentLoaded", setNavHeightToScreenHeight);
