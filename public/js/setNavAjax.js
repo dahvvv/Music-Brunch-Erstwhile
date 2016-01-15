9b17d44e7110ddc77f7e277efafa8d5ab3f82859
Dave Koenig <davidjosephkoenig@gmail.com>
2016-01-14 21:48:40 -0500

    move event listener to nav

--- public/js/setNavAjax.js
+++ public/js/setNavAjax.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-	Array.prototype.forEach.call(document.links, function(link) {
-		link.addEventListener("click", function(e) {
-			e.preventDefault();
-			history.pushState({path:link.pathname},"",link.pathname);
-			var request = new XMLHttpRequest();
-			request.onreadystatechange = function() {
-				if (request.readyState == 4 && request.status == 200) {
-					var main = document.getElementById("main");
-					main.innerHTML = request.response;
-				}
-			};
-			request.open("GET", link.pathname);
-			request.send(null);
-		});
+	var nav = document.getElementsByTagName("nav")[0];
+	nav.addEventListener("click", function(e) {
+		e.preventDefault();
+		var link = e.target;
+		while (link.tagName != "A" && link.tagName != "HTML") {
+			link = link.parentNode;
+		}
+		history.pushState({path:link.pathname},"",link.pathname);
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if (request.readyState == 4 && request.status == 200) {
+				var main = document.getElementById("main");
+				main.innerHTML = request.response;
+			}
+		};
+		request.open("GET", link.pathname);
+		request.send(null);
 	});
 });
