d80fdc50c3713e8591f593730ad635ec5f7eafc2
Dave Koenig <davidjosephkoenig@gmail.com>
2016-01-15 18:10:25 -0500

    real back button functionality!

--- public/js/setNavAjax.js
+++ public/js/setNavAjax.js
@@ -6,12 +6,16 @@ document.addEventListener("DOMContentLoaded", function() {
 		while (link.tagName != "A" && link.tagName != "HTML") {
 			link = link.parentNode;
 		}
-		history.pushState({path:link.pathname},"",link.pathname);
 		var request = new XMLHttpRequest();
 		request.onreadystatechange = function() {
 			if (request.readyState == 4 && request.status == 200) {
 				var main = document.getElementById("main");
 				main.innerHTML = request.response;
+				history.pushState({mainHTML:request.response},"",link.pathname);
+				window.onpopstate = function(e) {
+					var initHTML = "<h1>Erstwhile Records</h1>";
+					main.innerHTML = e.state ? e.state.mainHTML : initHTML;
+				};
 			}
 		};
 		request.open("GET", link.pathname);
