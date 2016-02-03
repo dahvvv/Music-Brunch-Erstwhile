71244527bb2bc66103ccae12afd9744f55b19923
Dave Koenig <davidjosephkoenig@gmail.com>
2016-02-03 06:50:14 -0500

    refactor pushstate

--- public/js/setNavAjax.js
+++ public/js/setNavAjax.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 	var nav = document.getElementsByTagName("nav")[0];
+	var main = document.getElementById("main");
+	var audio = document.getElementsByTagName("audio")[0];
 	nav.addEventListener("click", function(e) {
 		e.preventDefault();
 		var link = e.target;
@@ -9,10 +11,20 @@ document.addEventListener("DOMContentLoaded", function() {
 		var request = new XMLHttpRequest();
 		request.onreadystatechange = function() {
 			if (request.readyState == 4 && request.status == 200) {
-				var main = document.getElementById("main");
-				main.innerHTML = request.response;
-				history.pushState({mainHTML:request.response},"",link.pathname);
+				var res = JSON.parse(request.response);
+				audio.setAttribute("src", "");
+				audio.load();
+				audio.parentNode.removeChild(audio);
+				audio = document.createElement("audio");
+				audio.controls = true;
+				audio.preload = "metadata";
+				audio.setAttribute("src", res.src);
+				audio.load();
+				document.body.appendChild(audio);
+				main.innerHTML = res.show;
+				history.pushState({mainHTML:res.show},"",link.pathname);
 				window.onpopstate = function(e) {
+					audio.set
 					var initHTML = "<h1>Erstwhile Records</h1>";
 					main.innerHTML = e.state ? e.state.mainHTML : initHTML;
 				};
@@ -22,3 +34,8 @@ document.addEventListener("DOMContentLoaded", function() {
 		request.send(null);
 	});
 });
+
+/*
+GOAL:  on clicking a link, reset the audio's src property and then load() the audio.
+
+*/
